9ed1711528c0dcc54e59934c92536f3d23f953a9
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-03-17 18:21:41 +0800

    Update test-1-p.sh

--- smpackage/test-1-p.sh
+++ smpackage/test-1-p.sh
@@ -88,6 +88,8 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/poweroff
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+# 手动添加v2ray-plugin并在主程序中删除small中v2ray-plugin
 sed -i '$a src-git v2ray-plugin https://github.com/kiddin9/openwrt-packages/tree/master/v2ray-plugin' feeds.conf.default
 
 

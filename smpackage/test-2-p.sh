7e09fd30b75874d7bd873f780ddfa0133995f396
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-03-19 18:26:13 +0800

    Update test-2-p.sh

--- smpackage/test-2-p.sh
+++ smpackage/test-2-p.sh
@@ -82,8 +82,11 @@ cp -rf ../../kiddin9/luci-app-diskman/* luci-app-diskman
 
 # 去广告
 #ADGuardHome（kiddin9）
-mkdir luci-app-adguardhome
-cp -rf ../../kiddin9/luci-app-adguardhome/* luci-app-adguardhome
+#mkdir luci-app-adguardhome
+#cp -rf ../../kiddin9/luci-app-adguardhome/* luci-app-adguardhome
+
+svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome
 
 popd
 
@@ -148,6 +151,8 @@ CONFIG_PACKAGE_luci-app-diskman_INCLUDE_lsblk=y
 CONFIG_PACKAGE_luci-app-diskman_INCLUDE_mdadm=y
 CONFIG_PACKAGE_luci-app-diskman_INCLUDE_kmod_md_raid456=y
 CONFIG_PACKAGE_luci-app-diskman_INCLUDE_kmod_md_linear=y
+# 去广告
+CONFIG_PACKAGE_luci-app-adguardhome=y
 
 " >> .config
 

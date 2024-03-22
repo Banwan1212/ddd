d7e4dc15544aeb4415520e15337753e996444443
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-03-22 16:50:43 +0800

    Update test-2-p.sh

--- smpackage/test-2-p.sh
+++ smpackage/test-2-p.sh
@@ -17,6 +17,9 @@
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
+# sed -i 's/Authorization Required/wanuse.com/g' feeds/luci/modules/luci-base/ucode/template/sysauth.ut
+sed -i 's/Authorization Required/openwrt-23.05.2 wanuse.com/g' feeds/luci/themes/luci-theme-bootstrap/htdocs/luci-static/resources/view/bootstrap/sysauth.js
+sed -i 's/需要授权/openwrt-23.05 wanuse.com/g' feeds/luci/modules/luci-base/po/zh_Hans/base.po
 
 # 替换终端为bash
 # sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd

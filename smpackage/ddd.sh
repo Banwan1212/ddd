347507f83fe54ddaa19f1f85532339981e8346cd
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-03-22 23:08:21 +0800

    Update ddd.sh

--- smpackage/ddd.sh
+++ smpackage/ddd.sh
@@ -151,8 +151,9 @@ cp -rf ../../kiddin9/luci-app-onliner/* luci-app-onliner
 # 监控CPU性能
 mkdir netdata
 mkdir luci-app-netdata
-cp -rf ../../kiddin9/luci-app-netdata/* luci-app-netdata
+# cp -rf ../../kiddin9/luci-app-netdata/* luci-app-netdata
 cp -rf ../../kiddin9/netdata/* netdata
+git clone https://github.com/sirpdboy/luci-app-netdata luci-app-netdata
 # 网络共享（Samba4）
 mkdir luci-app-samba4
 cp -rf ../../kiddin9/luci-app-samba4/* luci-app-samba4
